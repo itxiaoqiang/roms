453fcb26c0a446d2ddb30956e167810cda70bbbb
Yanqiang Wang <itxiaoqiang@gmail.com>
2023-02-28 19:45:10 +0800

    Update Linux-ifort.mk
    change mpif90 to  mpiifort

--- Compilers/Linux-ifort.mk
+++ Compilers/Linux-ifort.mk
@@ -196,7 +196,7 @@ endif
 ifdef USE_MPI
          CPPFLAGS += -DMPI
  ifdef USE_MPIF90
-               FC := mpif90
+               FC := mpiifort
  else
              LIBS += -lfmpi-pgi -lmpi-pgi
  endif
